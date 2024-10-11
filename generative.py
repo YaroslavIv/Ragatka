05a7ecf7e72980ba6641c2d55bc54cd803d46568
Yric <yroslavcr@gmail.com>
2024-10-11 23:55:20 +0300

    add generative: Transformers, model: meta-llama/Llama-3.2-1B-Instruct

--- generative.py
+++ generative.py
@@ -1,7 +1,9 @@
+import torch
+import transformers
+from openai import OpenAI
 from abc import ABC, abstractmethod
 
 from registry import GENERATIVE_MODEL_REGISTRY
-from openai import OpenAI
 
 class Generative(ABC):
 
@@ -32,3 +34,36 @@ class APIOpenAI(Generative):
         )
 
         return response.choices[0].message.content
+
+@GENERATIVE_MODEL_REGISTRY.register_module
+class Transformers(Generative):
+    def __init__(self, model: str, torch_dtype: str, device_map: str) -> None:
+        self.model = model
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model=self.model,
+            torch_dtype=eval(torch_dtype), 
+            device_map=device_map
+        )
+        self.terminators = [
+            self.pipeline.tokenizer.eos_token_id,
+            self.pipeline.tokenizer.convert_tokens_to_ids("") or self.pipeline.tokenizer.eos_token_id
+        ]
+    
+    def generative_text(self, doc: str, quastion: str, max_length_doc: int = 1024) -> str:
+        content = [{"role": "user", "content": quastion}]
+        if len(doc) > 0:
+            content = [{"role": "system", "content": doc}] + content
+        
+        prompt = self.pipeline.tokenizer.apply_chat_template(
+            content, tokenize=False, add_generation_prompt=True
+        )
+        
+        outputs = self.pipeline(
+            prompt,
+            max_new_tokens=max_length_doc,
+            eos_token_id=self.terminators[0],  # Используем первый элемент, если он валиден
+            do_sample=True,
+        )
+
+        return outputs[0]["generated_text"][len(prompt):]
